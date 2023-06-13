b58058205c0a7df996ccc179b0fcdf6569cfa227
yaarian-om <dip.kumar020@gmail.com>
2023-06-13 11:20:53 +0600

    Uploading First Project File EXP1

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminModule } from './admin/admin.module';
-
-@Module({
-  imports: [AdminModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
